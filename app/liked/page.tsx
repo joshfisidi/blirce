e836026de09cf266510a6966584582a65fbcb5ba
joshfisidi <josh@fisidi.com>
2025-03-25 15:25:44 -0400

    fine tuning

--- app/liked/page.tsx
+++ app/liked/page.tsx
@@ -24,7 +24,7 @@ const Liked = async () => {
                         </div>
                         <div className="flex flex-col gap-y-2 mt-4 md:mt-0">
                             <p className="hidden md:block font-semibold text-sm">Playlist</p>
-                            <h1 className="text-white text-4xl sm:text-5xl lg:text-7xl font-bold">Liked Songs</h1>
+                            <h1 className="text-white text-4xl sm:text-5xl lg:text-7xl font-bold">Liked Music</h1>
                         </div>
                     </div>
                 </div>
